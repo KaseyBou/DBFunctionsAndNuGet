64d32b939e46b75e1c68f5e1676a4cf1b7486fc1
Kasey Bourdier <kbourdier@email.neit.edu>
2021-03-05 19:50:32 -0500

    Add project files.

---
+++ BlockBuster/BlockBusterBasicFunctions.cs
@@ -0,0 +1,83 @@
+﻿using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlockBuster
+{
+    public class BlockBusterBasicFunctions
+    {
+        public static Movie GetMovieById(int id)
+        {
+            using(var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies.Find(id);
+            }
+        }
+
+        public static List<Movie> GetAllMovies()
+        {
+            using(var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies.ToList();
+            }
+        }
+
+        public static List<Movie> GetAllCheckedOutMovies()
+        {
+            using(var db = new SE407_BlockBusterContext())
+            {
+                return db.Movies
+                    .Join(db.Transactions,
+                    m => m.MovieId,
+                    t => t.Movie.MovieId,
+                    (m, t) => new
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId,
+                        CheckedIn = t.CheckedIn
+                    }).Where(w => w.CheckedIn == "N")
+                        .Select(m => new Movie
+                        {
+                            MovieId = m.MovieId,
+                            Title = m.Title,
+                            ReleaseYear = m.ReleaseYear,
+                            GenreId = m.GenreId,
+                            DirectorId = m.DirectorId
+                        }).ToList();
+            }
+
+        }
+
+        public static List<Genre> GetAllMoviesByGenreDescription()
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Genres
+                    .Select(e => new Genre
+                    {
+                    GenreId = e.GenreId,
+                    GenreDescr = e.GenreDescr
+                    }).ToList();
+             }
+        }
+
+        public static List<Director> GetAllMoviesByDirectorLastName()
+        {
+            using (var db = new SE407_BlockBusterContext())
+            {
+                return db.Directors
+                    .Select(e => new Director
+                    {
+                        DirectorId = e.DirectorId,
+                        LastName = e.LastName
+                    }).ToList();
+            }
+        }
+
+    }
+}
---
+++ BlockBusterTest/BlockBusterBasicFunctionsTest.cs
@@ -0,0 +1,53 @@
+﻿
+using BlockBuster;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BlockBusterTest
+{
+   public class BlockBusterBasicFunctionsTest
+    {
+        [Fact]
+        public void getMovieByIdTest()
+        {
+            var result = BlockBusterBasicFunctions.GetMovieById(11);
+
+            Assert.True(result.Title == "Vertigo");
+            Assert.True(result.ReleaseYear == 1958);
+        }
+
+        [Fact]
+        public void GetAllMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMovies();
+
+            Assert.True(result.Count == 50);
+        }
+
+        [Fact]
+        public void GetAllCheckedOutMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllCheckedOutMovies();
+
+            Assert.True(result.Count == 3);
+        }
+
+        [Fact]
+        public void GetAllMoviesByGenreDescription()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMoviesByGenreDescription();
+
+            Assert.True(result.Count == 50);
+        }
+
+        [Fact]
+        public void GetAllMoviesByDirectorLastName()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMoviesByDirectorLastName();
+
+            Assert.True(result.Count == 50);
+        }
+    }
+}
