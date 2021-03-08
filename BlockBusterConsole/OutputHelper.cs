454cb433bb43643749a3782dbd64d3004b535428
Kasey Bourdier <kbourdier@email.neit.edu>
2021-03-07 19:39:52 -0500

    output as csv or to the console.

---
+++ BlockBusterConsole/OutputHelper.cs
@@ -0,0 +1,33 @@
+﻿using BlockBuster;
+using BlockBuster.Models;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace BlockBusterConsole
+{
+    class OutputHelper
+    {
+        public void WriteToConsole(List<Movie> movies)
+        {
+            Console.WriteLine("List of Movies");
+
+            foreach(var m in movies)
+            {
+                Console.WriteLine($"MovieID: {m.MovieId}     Title: {m.Title}     Release Year: {m.ReleaseYear}");
+            }
+        }
+
+        public void WriteToCSV(List<Movie> movies)
+        {
+            using (var writer = new StreamWriter(@"..\file.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(movies);
+            }
+        }
+    }
+}
---
+++ BlockBusterConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using BlockBuster;
+using System;
+
+namespace BlockBusterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = BlockBusterBasicFunctions.GetAllMovies();
+            var oh = new OutputHelper();
+            string ans;
+            bool incorrect = true;
+
+            Console.WriteLine("would you like the output as a CSV or written into the console? ( type: CSV or cons)");
+          
+
+            do {
+                ans = Console.ReadLine();
+
+                if (ans == "CSV")
+                {
+                    oh.WriteToCSV(b);
+                    incorrect = false;
+                }
+                else if (ans == "cons")
+                {
+                    oh.WriteToConsole(b);
+                    incorrect = false;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR* Not a valid input please try again and type CSV or cons");
+                }
+
+            } while (incorrect);
+               
+        }
+    }
+}
